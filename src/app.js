b8e7b52dd861083cb61c86a221167784a3d18bae
lwyers01 <lwyers01@qub.ac.uk>
2022-10-04 20:50:07 +0100

    Updated a few of the classes, need to add an employee class. These will contribute to Team scores.

--- src/app.js
+++ src/app.js
@@ -4,23 +4,57 @@
 
 //Classes
 
+// Project class - stores deatils about the size of a project, its budget, where it is a legacy project
+
 class Project {
 
     constructor(name, teams, budget, profit, isLegacy, size, custRelScore) {
+        //Project name, randomly generated
         this.name = name;
+        //array of different teams, not all projects will have the same teams 
         this.teams = teams;
+        //budget allocated to project per year
         this.budget = budget;
+        //profit generated from project
         this.profit = profit;
+        //this will have an impact on complexity (developers less likely to be less comfortable with code not created in house, following standardised structure) 
         this.isLegacy = isLegacy;
+        // scope of website, database, structure, features, users
         this.size = size;
+        // customer satisfaction, hard to 
         this.custRelScore = custRelScore;
     }
 }
 
+
+// creating an array of 'Owned Projects'
+
+//need to populate the company - will eventually be a .json file, but for now keeping it simple and adding here
+
+// Teams
+
+//Developer Teams
+
+
+let devTeam1 = new Development(2, 1, 2.5);
+devTeam1.resources = devTeam1.senDevCount + devTeam1.junDevCount;
+
+
+
+class Employee {
+
+    constructor(department,)
+
+}
+
 class Team {
-    constructor(name, resources, effeciency, moral, quality, synergy, knowledge, experience, leadership, taskResolution) {
-        this.name = name;
+    //effeciency, moral, quality, synergy, knowledge, experience, leadership,
+    constructor(type, resources, taskResolution) {
+        this.type = type;
         this.resources = resources;
+        this.employees = employees;
+        /*
+        For these need to make something actually definable in a value way - these are too subjective
         this.effeciency = effeciency;
         this.moral = moral;
         this.quality = quality;
@@ -28,17 +62,24 @@ class Team {
         this.knowledge = knowledge;
         this.experience = experience;
         this.leadership = leadership;
+        */
         this.taskResolution = taskResolution;
     }
 }
 
+
+
 class Development extends Team {
-    constructor(senDevCount, junDevCount, specRealis, codeEff, codeReu, bugCreation) {
+    //specRealis, codeEff, codeReu
+    constructor(senDevCount, junDevCount, bugCreation) {
         this.senDevCount = senDevCount;
         this.junDevCount = junDevCount;
+        /*
+        For these need to make something actually definable in a value way - these are too subjective
         this.specRealis = specRealis;
         this.codeEff = codeEff;
         this.codeReu = codeReu;
+        */
         this.bugCreation = bugCreation;
     }
 }
@@ -137,17 +178,114 @@ function daily() {
 }
 
 
+
+
 // make random funcions for events
 
+// Draft attempt at events
 
-//alocation of resources
-//training needed
-// risk assesed
-// pricing and potential profits
-// proposed additional features
-//research into pre-existing app
-//potential restructuring/new developments needed
+// Big Events:
 
+// Company Requests Services
+
+// //Potential Factors
+// 1. Are they looking to create a whole new system? 
+// 2. Will this be a legacy based system?
+
+// //if new system
+// 1. Allocation of resources
+// 2. Deciding on technologies
+// 3. Software archeticture
+// 4. Research of field
+// 5. Risks assesed
+// 6. Pricing assesed
+// 7. Market Research
+// 8. Features discussion
+
+// //if legacy
+// 1. Allocation of resources
+// 2. New training needed? 
+// 3. Risk assesed
+// 4. Pricing and potential profits
+// 5. Proposed additional features
+// 6. Research into pre-existing app
+// 7. Potential restructuring/new developments needed
+
+// Lead to Negotiations if company sees if it is viable
+// 1. Produce proposal
+// 2. Initiate negotiations
+// 3. Conduct Negotiations
+// 4. Update proposal
+// 5. Reassess resources
+// 6. Reassess potential pricing
+// 7. Reassess potential risk
+// 8. Reassess features
+// 9. Reassess profits
+
+// Either negotions go through different phases, or are dropped completely
+
+// If company decideds to go through with project
+
+// Allocation of resources
+// 1. Development team assigned
+// 2. Testing Team assigned
+// 3. Product Owner Assigned
+// 4. Project manager assigned
+// 5. UX/UI Team assigned
+// 6. Marketing consulted
+
+// Development plan
+// 1. Key milestones decided
+// 2. Budget
+// 3. Feature Development
+// 4. Customer Feedback schedule
+// 5. Prototype timeline
+// 6. Methodology
+// 7. Software consideration
+// 8. Hardware Considerations
+// 9. Hosting
+// 10. Feature criticality
+
+// Development Processes
+// 1. Daily standup - work assesed
+// 2. Ticket Creation
+// 3. Ticket Completion
+// 4. QA Testing tickets
+// 5. Bug discovery
+
+// Sprint Initiated
+
+// Sprint Finished
+
+// Further Testing
+
+// Feature Creation
+
+// Feature sent to Client
+// 1. Client feedback
+// 2. Bug discovery
+// 3. Feature Reassesment
+
+// Ongoing Client Relationships With Project Owner
+
+// 1. Client feedback
+// 2. Deliverables
+
+// Prototype developed
+
+// Further Testing
+
+// Product Complete
+
+// //Preexisting Contracts
+
+// Client Relations
+// 1. Bugs encountered
+// 2. Customer Service
+// 3. Support Team proficiency
+// 4. Stability of products
+// 5. Additional features needed
+// 6. Continuing Development
 
 
 
